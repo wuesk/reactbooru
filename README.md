bf4c5bd48d084bc1cc08ace1b8d04c0f859b0258
wuesk <ws@jesx.dev>
2025-03-18 20:07:22 +0100

    progress

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Reactbooru
+<pre>
+<center>
+╭━━━┳━━━┳━━━┳━━━┳━━━━┳━━╮╭━━━┳━━━┳━━━┳╮╱╭╮
+┃╭━╮┃╭━━┫╭━╮┃╭━╮┃╭╮╭╮┃╭╮┃┃╭━╮┃╭━╮┃╭━╮┃┃╱┃┃
+┃╰━╯┃╰━━┫┃╱┃┃┃╱╰┻╯┃┃╰┫╰╯╰┫┃╱┃┃┃╱┃┃╰━╯┃┃╱┃┃
+┃╭╮╭┫╭━━┫╰━╯┃┃╱╭╮╱┃┃╱┃╭━╮┃┃╱┃┃┃╱┃┃╭╮╭┫┃╱┃┃
+┃┃┃╰┫╰━━┫╭━╮┃╰━╯┃╱┃┃╱┃╰━╯┃╰━╯┃╰━╯┃┃┃╰┫╰━╯┃
+╰╯╰━┻━━━┻╯╱╰┻━━━╯╱╰╯╱╰━━━┻━━━┻━━━┻╯╰━┻━━━╯
+</center>
+</pre>
+This incredible project is a imageboard clone, similar to Safebooru and other tag-based anime picture websites. The goal of this project is to learn about storing a lot of images using Cloudflare's R2 and easly finding them using tags thanks to data stored in PSQL database. The application itself is written in Express.js with EJS used for templating.
+
+## Installation and running
+`npm i`
+`npm run dev`
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,25 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import postsRouter from './routes/posts';
 
-// Load environment variables
+import { errorHandler } from './middleware/errorHandler';
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Set view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
-
-// Static files middleware
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
-app.get('/', (req, res) => {
-  res.render('layouts/main', {
-    title: 'Reactbooru',
-    content: res.render('pages/index', {
-      posts: [] // We'll add posts data later
-    })
-  });
-});
+//Routes
+app.use('/posts', postsRouter);
+
+app.use(errorHandler);
 
 // Start server
 app.listen(port, () => {
